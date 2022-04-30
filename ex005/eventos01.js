73449bf76ac2491116cd61fda3c1213fbe48a5bf
Pedro Fratassi <pedrofratassi2@gmail.com>
2022-04-30 09:10:08 -0300

    Criação Lista de Exercício

---
+++ ex005/eventos01.js
@@ -0,0 +1,7 @@
+function mensagem(){
+    alert(`Olá eu sou uma mensagem do JS!`)
+ }
+
+ function mensagem2(){
+    alert(`Eu sou a mensagem do Mouse!`)
+ }
---
+++ ex007/valida.js
@@ -0,0 +1,9 @@
+function valida(){
+
+    window.document.getElementById('nome')
+    if(nome == ""){
+        window.console.log(`Esse campo não pode estar em vazio`)
+    } else {
+        window.console.log(`Seja Bem-Vindo ${nome}`)
+    }
+}
